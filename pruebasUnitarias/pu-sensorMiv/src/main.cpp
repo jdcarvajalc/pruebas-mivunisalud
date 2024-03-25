5c89875b8c356d8c2caebe3e6e31b4d2bb9d68be
jdcarvajal <jdcc1308@gmail.com>
2024-03-25 16:23:02 -0500

    Change pu-sensor.ino to pu-sensor.cpp
    This commit changes the file name from pu\_sensor.ino to pu\_sensor.cpp and updates the corresponding header. The code logic remains the same.

--- pruebasUnitarias/pu-sensorMiv/src/main.cpp
+++ pruebasUnitarias/pu-sensorMiv/src/main.cpp
@@ -1,28 +1,28 @@
 /**
- * @file   pu_sensor.ino
- * @brief  Prueba unitaria del sensor SHT31 con Arduino.
- *
- * Este programa realiza la prueba unitaria del sensor SHT31, midiendo la temperatura
- * y la humedad y mostrando los resultados en el monitor serie. Se utiliza la biblioteca
- * Adafruit_SHT31 para la comunicación con el sensor a través de I2C.
- *
- * @author Juan David Carvajal Cucuñame, María Paula Cabezas Charry
- * @date   Fecha de Creación
- */
+* @file   pu_sensor.cpp
+* @brief  Prueba unitaria del sensor SHT31 con Arduino.
+*
+* Este programa realiza la prueba unitaria del sensor SHT31, midiendo la temperatura
+* y la humedad y mostrando los resultados en el monitor serie. Se utiliza la biblioteca
+* Adafruit_SHT31 para la comunicación con el sensor a través de I2C.
+*
+* @author Juan David Carvajal Cucuñame, María Paula Cabezas Charry
+*/
 
 #include <Arduino.h>
 #include <Wire.h>
 #include "Adafruit_SHT31.h"
 
-Adafruit_SHT31 sht31 = Adafruit_SHT31(); /**< Objeto para interactuar con el sensor SHT31 */
+// Objeto para interactuar con el sensor SHT31
+Adafruit_SHT31 sht31 = Adafruit_SHT31(); 
 
 /**
- * @brief Inicialización del programa y configuración del sensor.
- *
- * Se establece la comunicación serial y se intenta inicializar el sensor SHT31.
- * Si no se puede encontrar el sensor, se imprime un mensaje de error y el programa
- * entra en un bucle infinito.
- */
+* @brief Inicialización del programa y configuración del sensor.
+*
+* Establece la comunicación serial e intenta inicializar el sensor SHT31.
+* Si no se puede encontrar el sensor, imprime un mensaje de error y el programa
+* entra en un bucle infinito.
+*/
 void setup() {
   Serial.begin(115200);
 
@@ -33,31 +33,31 @@ void setup() {
 }
 
 /**
- * @brief Bucle principal del programa.
- *
- * En cada iteración del bucle, se lee la temperatura y la humedad del sensor SHT31.
- * Si los valores son válidos, se imprimen en el monitor serie. En caso de que haya un
- * valor nulo, se imprime un mensaje de error correspondiente. Luego, el programa espera
- * 60 segundos antes de realizar la siguiente lectura.
- */
+* @brief Bucle principal del programa.
+*
+* En cada iteración del bucle, se lee la temperatura y la humedad del sensor SHT31.
+* Si los valores son válidos, se imprimen en el monitor serie. En caso de que haya un
+* valor nulo, se imprime un mensaje de error correspondiente. Luego, el programa espera
+* 60 segundos antes de realizar la siguiente lectura.
+*/
 void loop() {
-  float temp = sht31.readTemperature(); /**< Variable para almacenar la temperatura leída del sensor */
-  float hum = sht31.readHumidity();    /**< Variable para almacenar la humedad leída del sensor */
+  float temp = sht31.readTemperature();
+  float hum = sht31.readHumidity();
 
   if (!isnan(temp)) { 
     Serial.print("Temperatura(°C): "); 
     Serial.print(temp); 
     Serial.print("\t\t");
   } else { 
-    Serial.println("Valor nulo de temperatura"); /**< Mensaje de error si la temperatura es nula */
+    Serial.println("Valor nulo de temperatura");
   }
   
   if (!isnan(hum)) {  
     Serial.print("Humedad(%): "); 
     Serial.println(hum);
   } else { 
-    Serial.println("Valor nulo de humedad"); /**< Mensaje de error si la humedad es nula */
+    Serial.println("Valor nulo de humedad");
   }
 
-  delay(60000); /**< Espera 60 segundos antes de realizar la siguiente lectura */
+  delay(60000);
 }
