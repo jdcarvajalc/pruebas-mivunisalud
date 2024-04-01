1aa5d7055755ca1d21accfadf25442d0713475de
JuanDavid1321 <jdcarvajal@unicauca.edu.co>
2024-04-01 10:54:56 -0500

    Agregando prueba unitaria de componente WiFi

---
+++ pruebasUnitarias/pu-wifiMiv/src/main.cpp
@@ -0,0 +1,37 @@
+/**
+* @file   main.cpp
+* @brief  Prueba unitaria del componente de conexión WiFi del ESP32.
+*
+* Este programa realiza una prueba unitaria del componente de conexión WiFi de la ESP32.
+* Se conecta a una red WiFi especificada por el SSID y la contraseña, y luego imprime la
+* dirección IP local de la ESP32 en el monitor serie.
+*
+* @author Juan David Carvajal Cucuñame, María Paula Cabezas Charry
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+// SSID y contraseña de la red WiFi
+const char* ssid = "NombreDeTuRed";
+const char* password = "TuContrasena";
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("Conectando a WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("¡WiFi conectado!");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP().toString());
+}
+
+void loop() {}
