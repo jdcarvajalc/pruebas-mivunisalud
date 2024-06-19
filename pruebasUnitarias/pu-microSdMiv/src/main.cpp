7d374ac5dc61e35301b22b814082745968e2145e
jdcarvajalc <jdcc1308@gmail.com>
2024-06-19 08:19:49 -0500

    update platformio.ini and main.cpp files in pu-microSdMiv project, and modified repoPruebas.code-workspace file to point to pu-microSdMiv directory

--- pruebasUnitarias/pu-microSdMiv/src/main.cpp
+++ pruebasUnitarias/pu-microSdMiv/src/main.cpp
@@ -1,62 +1,68 @@
 /**
-* @file   main.cpp
-* @brief  Prueba unitaria del módulo MicroSD con la ESP32.
-*
-* Este programa realiza una prueba unitaria del módulo MicroSD, escribiendo la frase
-* "Funciona bien" en un archivo llamado "prueba.txt". Se utiliza la biblioteca SD
-* para la gestión de la tarjeta.
-*
-* @author Juan David Carvajal Cucuñame, Maria Paula Cabezas Charry
-*/
+ * @file   main.cpp
+ * @brief  Prueba unitaria del módulo MicroSD con la ESP32.
+ *
+ * Este programa realiza una prueba unitaria del módulo MicroSD, escribiendo la frase
+ * "Funciona bien" en un archivo llamado "prueba.txt". Se utiliza la biblioteca SD
+ * para la gestión de la tarjeta.
+ *
+ * @author Juan David Carvajal Cucuñame, Maria Paula Cabezas Charry
+ */
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <SD.h>
 
 // Pin de selección del módulo MicroSD
-const int chipSelect = 5;
+const int CHIP_SELECT = 5;
 
 /**
-* @brief Inicialización del programa y configuración del módulo MicroSD.
-*
-* Se establece la comunicación serial y se intenta inicializar la tarjeta MicroSD.
-* Si no se puede inicializar, se imprime un mensaje de error y el programa se detiene.
-*/
-void setup() {
+ * @brief Inicialización del programa y configuración del módulo MicroSD.
+ *
+ * Se establece la comunicación serial y se intenta inicializar la tarjeta MicroSD.
+ * Si no se puede inicializar, se imprime un mensaje de error y el programa se detiene.
+ */
+void setup()
+{
   Serial.begin(115200);
 
   // Inicialización del módulo MicroSD
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(CHIP_SELECT))
+  {
     Serial.println("Error al inicializar la tarjeta MicroSD");
-    while (1);
+    while (1)
+      ;
   }
 
   Serial.println("Tarjeta MicroSD inicializada correctamente");
-  
 }
 
 /**
-* @brief Bucle principal del programa.
-*
-* Abre el archivo "prueba.txt" en modo de escritura, escribe la frase "Funciona bien"
-* y luego cierra el archivo. El programa espera 5 segundos antes de realizar la
-* siguiente escritura.
-*/
-void loop() {
+ * @brief Bucle principal del programa.
+ *
+ * Abre el archivo "prueba.txt" en modo de escritura, escribe la frase "Funciona bien"
+ * y luego cierra el archivo. El programa espera 5 segundos antes de realizar la
+ * siguiente escritura.
+ */
+void loop()
+{
   // Abre el archivo en modo de escritura
   File dataFile = SD.open("/prueba.txt", FILE_WRITE);
 
   // Verifica si se pudo abrir el archivo correctamente
-  if (dataFile) {
+  if (dataFile)
+  {
     Serial.println("Escribiendo en el archivo: prueba.txt");
-    
+
     // Escribe la frase en el archivo
     dataFile.println("Funciona bien");
 
     // Cierra el archivo
     dataFile.close();
     Serial.println("Escritura completada");
-  } else {
+  }
+  else
+  {
     Serial.println("Error al abrir el archivo");
   }
 
